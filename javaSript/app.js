f84470bf191f3f169bfcabeb74f165bd83f8b640
GARVIT THAKRAL <thakralgarvit1@gmail.com>
2024-10-18 21:11:27 +0530

    to do simple app

--- javaSript/app.js
+++ javaSript/app.js
@@ -1,8 +1,30 @@
-let movieName = prompt("plese enter your guess or write quit to exit the game: ");
-let favMovie = "ironman";
-while((movieName != favMovie) && (movieName != "quit")){
-    movieName = prompt("wrong guess plese enter your guess or write quit to exit the game: ");
-}
-if(movieName == favMovie){
-    console.log("congoo")
+let todo = [];
+
+let req = prompt("plese enter your request");
+
+while(true) {
+    if(req == "quit") {
+        console.log("quitting the app");
+        break;
+    }
+
+    if(req == "list") {
+        console.log("------------");
+        for(let i = 0; i<todo.length; i++){
+            console.log(i, todo[i]);
+        }
+        console.log("------------");
+    } else if(req == "add") {
+        let task = prompt("plese add your task");
+        todo.push(task);
+        console.log("item added");
+    } else if(req == "delete"){
+        let idx = prompt("plese enter the index u want to delete");
+        todo.splice(idx, 1);
+        console.log("task deleted");
+    } else {
+        console.log("wrong respond");
+    }
+
+    req = prompt("plese enter your request");
 }
