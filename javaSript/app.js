a92ed33bd132adb01ec0abcc9a4e01381dd9cbda
GARVIT THAKRAL <thakralgarvit1@gmail.com>
2024-12-28 15:55:29 +0530

    Random color selector

--- javaSript/app.js
+++ javaSript/app.js
@@ -1,40 +1,21 @@
-let para1 = document.createElement('p');
-para1.innerText = "hey i am red!!";
-document.querySelector("body").append(para1);
-para1.classList.add("red");
+let btn = document.querySelector("button");
 
-let high = document.createElement('h3');
-high.innerText =  "hey i am h3!!"
-document.querySelector("body").append(high);
-high.classList.add("green");
+btn.addEventListener("click", function() {
+    let h3 = document.querySelector("h3");
+    let randomcolor = getrandome();
+    h3.innerText = randomcolor;
 
-let div = document.createElement('div');
-let h1 = document.createElement('h1');
-let p = document.createElement('p');
+    let div = document.querySelector("div");
+    div.style.backgroundColor = randomcolor;
 
-h1.innerText = "i am in div";
-p.innerText = "me too";
+    console.log("colour updated");
+});
 
-document.querySelector("body").append(div);
-document.querySelector("div").append(h1, p);
+function getrandome() {
+    let R =Math.floor(Math.random() * 255);
+    let G =Math.floor(Math.random() * 255);
+    let B =Math.floor(Math.random() * 255);
 
-div.classList.add("boder");
-
-let btn = document.createElement("button");
-let inner = document.createElement("input");
-document.querySelector("body").append(btn, inner);
-btn.innerText = "click me";
-
-btn.setAttribute("id", "btn");
-inner.setAttribute("placeholder", "username");
-
-btn.classList.add("text");
-
-let h2 = document.createElement("h1");
-h2.innerText = "DOM PRACTICE"
-h2.classList.add("under");
-document.querySelector("body").append(h2);
-
-let pp = document.createElement("p");
-pp.innerHTML = "Apna Collage <b>Delta</b> Practice";
-document.querySelector("body").append(pp);
+    let color = `rgb(${R}, ${G}, ${B})`;
+    return color;
+}
