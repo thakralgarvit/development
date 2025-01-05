3d451f09265b7befc562a638e1d70a5923f6d924
GARVIT THAKRAL <thakralgarvit1@gmail.com>
2025-01-05 07:46:56 +0530

    Promise

--- javaSript/app.js
+++ javaSript/app.js
@@ -1,78 +1,25 @@
-let gameSql = []; // to track game Squance
-let playerSql = []; // to track player Squance
+let h1 = document.querySelector("h1");
 
-let started = false; // helps to monitor if the game has started
-let level = 0; // counts level
+// chnage color with 1 sec delay from each other with the help of promise
 
-let btns = ["red", "yel", "green", "pur"]; // to store all the ans through which computer will genrate the color
-let h2 = document.querySelector("h2");
+function colourchn(color, delay) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            h1.style.color = color;
+            resolve("color changed");
+        },delay);
+    });
+}
 
-document.addEventListener("keypress", function() {
-    if (started == false) {
-        console.log("Game Started");  // to start the game we need top change the value of the started to true.
-        started = true;
-        levelup(); 
-    }
-    
+colourchn("red", 1000)
+.then(() => {
+    console.log("red");
+    return colourchn("blue", 1000);
+})
+.then(() => {
+    console.log("blue");
+    return colourchn("green", 1000);
+})
+.then(() => {
+    console.log("green");
 });
-
-function btnFlash(btn) {
-    btn.classList.add("flash");
-    setTimeout(function() {
-        btn.classList.remove("flash");
-    }, 300);
-}
-
-function userFlash(btn) {
-    btn.classList.add("userflash");
-    setTimeout(function() {
-        btn.classList.remove("userflash");
-    }, 300);
-}
-
-function levelup() {
-    playerSql =[];
-    level++;
-    h2.innerText = `levele ${level}`
-
-    let randidx = Math.floor(Math.random() * 3);
-    let randcolo = btns[randidx];
-    let randbtn = document.querySelector(`.${randcolo}`);
-    gameSql.push(randcolo);
-    console.log(gameSql);
-    btnFlash(randbtn);
-}
-
-function checkAns (idx) {
-    if (playerSql[idx] === gameSql[idx]){
-        if (playerSql.length == gameSql.length){
-            setTimeout(levelup, 500);
-        }
-    } else {
-        h2.innerText = `Game is over! Press Any Key to start.`;
-        reset();
-    }
-}
-
-function btnpress() {
-    let btn = this;
-    console.log("btn was pressed")
-    userFlash(btn);
-
-    let usercolo = btn.getAttribute("id");
-    playerSql.push(usercolo);
-
-    checkAns(playerSql.length-1);
-}
-
-let allbtns = document.querySelectorAll(".btn");
-for (btn of allbtns) {
-    btn.addEventListener("click", btnpress);
-}
-
-function reset() {
-    started = false;
-    gameSql = [];
-    playerSql = [];
-    level = 0;
-}
