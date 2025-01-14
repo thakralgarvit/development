eb7a0ad1914047e7f180cffc899444e51ba39943
GARVIT THAKRAL <thakralgarvit1@gmail.com>
2025-01-14 15:15:25 +0530

    very basic  of SQL data base

--- backend/index.js
+++ backend/index.js
@@ -1,32 +1,42 @@
 const express = require('express');
 const app = express();
 const path = require("path");
+const { faker } = require('@faker-js/faker');
+const mysql = require("mysql2");
+const { connect } = require('http2');
+
+const connection = mysql.createConnection( {
+    host: "localhost",
+    user: "root",
+    database: "delta_app",
+    password: "@veena100"
+});
 
 let port = 8080;
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views")); // so that our server run smoothly no matter where we start it from.
+app.use(express.static(path.join(__dirname, "/public"))); // so that our server run smoothly no matter where we start it from.
 
-app.listen(port, () => {
-    console.log("app is listening");
-});
+try {
+  connection.query("SHOW TABLES", (err, results) => {
+    if (err) throw err;
+    console.log(results);
+  });
+} catch (err) {
+  console.log(err);
+}
 
-app.get("/ig/:username", (req, res) => {
-    let {username} = req.params;
-    const instadata = require("./data.json");
-    const data = instadata[username];
-    if(data) {
-        res.render("insta.ejs", {data});
-    } else {
-        res.render("error.ejs");
-    }
-});
+connection.end();
 
-app.get("/", (req, res) => {
-    res.render("home.ejs");
-});
+let randuser = () => {
+    return {
+      id: faker.string.uuid(),
+      username: faker.internet.username(),
+      email: faker.internet.email(),
+      password: faker.internet.password(),
+    };
+  }
 
-app.get("/rolldice", (req, res) => {
-    let dice = Math.floor(Math.random() * 6) + 1;
-    res.render("rolldice.ejs", { num: dice});
-});
+
+  
